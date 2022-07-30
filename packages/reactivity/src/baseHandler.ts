1313f87708c4680ac61c977f83e03d9eefec6cb7
yolinBoy <1071989764@qq.com>
2022-07-30 17:09:47 +0800

    feat:【依赖收集】实现依赖收集

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,3 +1,4 @@
+import { track, trigger } from './effect'
 
 export const enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive',
@@ -5,15 +6,24 @@ export const enum ReactiveFlags {
 
 export const mutableHandlers = {
   get(target, key, receiver) {
-    if(key === ReactiveFlags.IS_REACTIVE) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
       return true;
     }
+
+    track(target, 'get', key)
+
     // return target[key]
     return Reflect.get(target, key, receiver);
   },
   set(target, key, value, receiver) {
+    const oldValue = target[key];
+    const result = Reflect.set(target, key, value, receiver);
+    if (oldValue !== value) {
+      trigger(target, 'set', key, value, oldValue);
+    }
+
     // target[key] = value
     // return true
-    return Reflect.set(target, key, value, receiver);
+    return result
   }
 }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -2,6 +2,8 @@
 export let activeEffect = undefined;
 
 class ReactiveEffect {
+  public parent = undefined;
+  public deps = []
   public active = true; // 这个effect默认激活
   constructor(public fn) {// 用户传递的参数也会放到this上
 
@@ -12,10 +14,11 @@ class ReactiveEffect {
     // 如果effect激活，就执行，并且依赖收集
     // 核心就是将当前的effect和稍后渲染的属性关联一起
     try {
+      this.parent = activeEffect
       activeEffect = this;
       this.fn(); // 当稍后调用取值操作的时候，就可以获取到这个全局的activeEffect了
     } finally {
-      activeEffect = undefined;
+      activeEffect = this.parent;
     }
     
   }
@@ -27,4 +30,42 @@ class ReactiveEffect {
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn); // 创建响应式的effect
   _effect.run(); // 默认执行一次
+}
+
+
+const targetMap = new WeakMap();
+export function track(target, type, key) {
+  if(!activeEffect) {
+    return 
+  }
+  let depsMap = targetMap.get(target)
+  if(!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if(!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+  let shouldTrack = !dep.has(activeEffect)
+  if(shouldTrack) {
+    dep.add(activeEffect)
+    activeEffect.deps.push(dep)
+  }
+
+
+}
+
+
+export function trigger(target, type, key, newValue, oldValue) {
+  const depsMap = targetMap.get(target) 
+  if(!depsMap) { // 触发的值不在模板中使用
+    return
+  }
+  const effects = depsMap.get(key)
+  effects && effects.forEach(effect => {
+    // 我们在执行effect的时候，又要执行自己，那么我们需要屏蔽掉自己
+    if(effect !== activeEffect) {
+      effect.run()
+    }
+  })
 }
